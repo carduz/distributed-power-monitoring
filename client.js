aaaa200b0e35cb4e67ebacdd4281f4cabbe086bb
Claudio Cardinale <cardinale.claudio@gmail.com>
2017-01-08 15:54:30 +0100

    [client] draft refactoring

--- client.js
+++ client.js
@@ -1,91 +1,11 @@
 /**
- * Created by claudio on 30/12/16.
+ * Created by claudio on 08/01/17.
  */
 "use strict";
-let client = require('socket.io-client');
 let csvStream = require('./csvStream');
+let clientLib = require('./clientLib');
 if(process.argv.length != 3){
     console.error('Usage node worker.js {master address}');
     process.exit();
 }
-
-let socket = client(process.argv[2]);
-
-socket.on('event', (data)=>{});
-socket.on('disconnect', ()=>{
-    console.log('connection closed by the server');
-    socket.close();
-});
-
-socket.on('connect', ()=>{
-    console.log('connected');
-    socket.emit('client', ['print']);
-});
-
-let workers = [];
-socket.on('workers', (data)=>{
-    console.log('workers received');
-    connectToWorkers(data).then(sendWorks);
-});
-
-function connectToWorkers(data){
-    "use strict";
-    return Promise.all(data.map((value)=>{
-        let res = workers.filter((value2)=>{
-           if(value2.address == value)
-               return true;
-        });
-        if(res.length)
-            return Promise.resolve(res[0]);
-        return connectPromise(value);
-    })).then((data)=>{
-        workers=data;
-        console.log('connected to workers');
-    });
-
-}
-
-//TODO load balancer "worker"
-//TODO insert try...catch to avoid problems
-
-//TODO sync with init, don't recall on change workers but adapat itself
-//TODO how to get the answer? maybe the master should have a list of tasks
-//TODO close all at the end
-function sendWorks(){
-    "use strict";
-    let i = 0;
-    let num = workers.length;
-    /*emulateLongStreaming((job)=>{
-        workers[i++%num].connection.emit('job', job);
-    });*/
-    //TODO header in stderr
-    csvStream((record)=>{
-        workers[i++%num].connection.emit('job', JSON.stringify(record));
-    });
-}
-
-let jobs = [
-    '5',
-    'test',
-    '9',
-    'test3'
-];
-function emulateLongStreaming(callback, jobId){
-    "use strict";
-    jobId = jobId || 0;
-    if(jobId<jobs.length)
-        setTimeout(()=>{
-            callback(jobs[jobId]);
-                emulateLongStreaming(callback, jobId+1);
-        },1000);
-}
-
-//TODO manage disconnection, retry reconnect?
-function connectPromise(address){
-    return new Promise((resolve, reject)=> {
-        let clientIO = client(address);
-        clientIO.on('connect', ()=> {
-            resolve({address: address, connection: clientIO});
-        });
-    });
-}
+clientLib(process.argv[2], ['print'], csvStream);
---
+++ clientLib.js
@@ -0,0 +1,69 @@
+/**
+ * Created by claudio on 30/12/16.
+ */
+"use strict";
+module.exports = function (address, functions, dataFuntion) {
+    let client = require('socket.io-client');
+
+    let socket = client(address);
+
+    socket.on('event', (data)=>{});
+    socket.on('disconnect', ()=>{
+        console.log('connection closed by the server');
+        socket.close();
+    });
+
+    socket.on('connect', ()=>{
+        console.log('connected');
+        socket.emit('client', functions);
+    });
+
+    let workers = [];
+    socket.on('workers', (data)=>{
+        console.log('workers received');
+        connectToWorkers(data).then(sendWorks);
+    });
+
+    function connectToWorkers(data){
+        "use strict";
+        return Promise.all(data.map((value)=>{
+            let res = workers.filter((value2)=>{
+               if(value2.address == value)
+                   return true;
+            });
+            if(res.length)
+                return Promise.resolve(res[0]);
+            return connectPromise(value);
+        })).then((data)=>{
+            workers=data;
+            console.log('connected to workers');
+        });
+
+    }
+
+    //TODO load balancer "worker"
+    //TODO insert try...catch to avoid problems
+
+    //TODO sync with init, don't recall on change workers but adapat itself
+    //TODO how to get the answer? maybe the master should have a list of tasks
+    //TODO close all at the end
+    function sendWorks(){
+        "use strict";
+        let i = 0;
+        let num = workers.length;
+        //TODO header in stderr
+        dataFuntion((record)=>{
+            workers[i++%num].connection.emit('job', JSON.stringify(record));
+        });
+    }
+
+    //TODO manage disconnection, retry reconnect?
+    function connectPromise(address){
+        return new Promise((resolve, reject)=> {
+            let clientIO = client(address);
+            clientIO.on('connect', ()=> {
+                resolve({address: address, connection: clientIO});
+            });
+        });
+    }
+};
